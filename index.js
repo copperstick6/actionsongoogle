2478d3463f904ec1fedee56bd13479c233371a2a
William Han <William.Han2@Honeywell.com>
2017-06-26 17:06:12 -0400

    added permissions files

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const ApiAiApp = require('actions-on-google').ApiAiApp
 
 const WELCOME_INTENT = 'input.welcome'; //this is the name rom the API.AI intent. check the API.AI event console.
-const ZIP_INTENT = 'input.zip'
+const LOCATION_INTENT = 'input.loc'
+const HERE_INTENT = 'input.here'
+const COMPLETE_INTENT  = 'input.accepted'
 exports.silicon = (req, res) => {
   const app = new ApiAiApp({ request: req, response: res});
   function welcomeIntent(app){
@@ -11,7 +13,7 @@ exports.silicon = (req, res) => {
     return
   }
 
-  function zipIntent(app){
+  function locationIntent(app){
     let zipCode = app.getArgument('zipVal')
     let city = app.getArgument('city')
     if(zipCode == null){
@@ -22,9 +24,25 @@ exports.silicon = (req, res) => {
     }
   }
 
+  function hereIntent(app){
+    let locPermission = app.SupportedPermissions.DEVICE_PRECISE_LOCATION
+    app.askForPermissions('To address you by name and know your location',[locPermission])
+  }
+
+  function gotLoc(app){
+    if(app.isPermissionGranted()){
+      let deviceCoordinates = app.getDeviceLocation().coordinates;
+      app.ask("Your coordinates are: Latitude: " + deviceCoordinates.Latitude + "Longitude: " + deviceCoordinates.Longitude)
+    }
+    else{
+      app.ask('Permission to access current location denied. Please either try again or utilize the other methods')
+    }
+  }
 
   let actionMap = new Map();
+  actionMap.set(COMPLETE_INTENT, gotLoc)
   actionMap.set(WELCOME_INTENT, welcomeIntent)
-  actionMap.set(ZIP_INTENT, zipIntent)
+  actionMap.set(HERE_INTENT, hereIntent)
+  actionMap.set(LOCATION_INTENT, locationIntent)
   app.handleRequest(actionMap);
 }
